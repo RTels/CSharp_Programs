7e594a4572a5b6be52f4bd05f0798f48a92ff38a
Rafael Teles <rrft@cin.ufpe.br>
2020-04-15 15:57:21 -0300

    Add files via upload

---
+++ AverageScore.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string input = "0";
+            int count = 0;
+            int total = 0;
+            int currentNumber = 0;
+
+            while(input != "-1")
+            {
+                Console.WriteLine("Last number was {0}", currentNumber);
+                Console.WriteLine("Please enter the next score:");
+                Console.WriteLine("Current amount of entries {0}", count);
+
+                input = Console.ReadLine();
+                if (input == "-1")
+                {
+                    Console.WriteLine("---------------------------------------------");
+                    double average = (double)total / (double)count;
+                    Console.WriteLine("The average score of your students is {0}", average);
+
+                }
+                if(int.TryParse(input, out currentNumber) && currentNumber > 0 && currentNumber <21)
+                {
+                    total += currentNumber;
+                }
+                else
+                {
+                    if (input != "-1" )
+                    {
+                        Console.WriteLine("Please enter a value between 1 and 20.");
+                    }
+                    continue;
+                }
+                count++;
+
+                
+
+
+
+               
+
+            }
+            Console.ReadLine();     
+
+
+
+
+        }
+
+       
+        
+
+            
+
+
+
+                    
+
+                    
+
+
+
+
+            
+
+        
+    }
+}
